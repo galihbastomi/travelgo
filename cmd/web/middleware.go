72e8e1ede095353b5ca6696dfda936547b94b79e
galihbastomi <gayogama@gmail.com>
2021-02-21 14:24:47 +0700

    Inital commit

---
+++ cmd/web/middleware.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+
+//NoSurve add CSRF protection to all POST requests
+func NoSurve(next http.Handler) http.Handler{
+	csrfHandler:= nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
+}
+
+//SessionLoad load and saves the sessoin on every request
+func SessionLoad(next http.Handler) http.Handler{
+	return session.LoadAndSave(next)
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+#TRAVELGO!
+
+This is repository for my GO lang project.
+
+- Built in GO 1.15
+- Uses the[ Chi router](github.com/go-chi/chi)
+- Uses [Alex Edwards](github.com/alexedwards/scs/v2) SCS session management
+- Uses [nosurf](github.com/justinas/nosurf)
